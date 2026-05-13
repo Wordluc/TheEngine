f837ab006099598265cc78b45eb288d0e39ab263
Wordluc <frangiamoreluca@gmail.com>
2026-05-13 20:08:12 +0200

    fix: quadtree

--- quadtree.go
+++ quadtree.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"game/base"
 )
 
@@ -12,83 +13,115 @@ type QuadTreeElement[t base.Number] interface {
 }
 
 type QuadTree struct {
-	eles           []QuadTreeElement[float32]
-	center         base.Vec[float32]
-	size           base.Vec[float32]
+	Elements       []QuadTreeElement[float32]
+	Pos            base.Vec[float32]
+	Size           base.Vec[float32]
+	hasSub         bool
 	higherQuadTree *QuadTree
-	top_left       *QuadTree
-	top_right      *QuadTree
-	bottom_left    *QuadTree
-	bottom_right   *QuadTree
+	Top_left       *QuadTree
+	Top_right      *QuadTree
+	Bottom_left    *QuadTree
+	Bottom_right   *QuadTree
 }
 
 const (
-	TOP_LEFT = iota
-	TOP_RIGHT
-	BOTTOM_LEFT
-	BOTTOM_RIGHT
+	TOP_LEFT     = "TOP_LEFT"
+	TOP_RIGHT    = "TOP_RIGHT"
+	BOTTOM_LEFT  = "BOTTOM_LEFT"
+	BOTTOM_RIGHT = "BOTTOM_RIGHT"
 )
 
-func NewQuadTree(center, size base.Vec[float32], higher *QuadTree) *QuadTree {
+func NewQuadTree(pos, size base.Vec[float32], higher *QuadTree) *QuadTree {
 	res := QuadTree{}
-	res.center = center
-	res.size = size
-	res.eles = make([]QuadTreeElement[float32], 0)
+	res.Pos = pos
+	res.Size = size
+	res.Elements = make([]QuadTreeElement[float32], 0)
 	res.higherQuadTree = higher
 	return &res
 }
 
 func (q *QuadTree) Insert(e QuadTreeElement[float32]) error {
-	x, y := e.GetPos().Get()
-	w, h := e.GetBox().Get()
-	quadTrees := []int{}
-	if x < q.center.X && y < q.center.Y {
-		quadTrees = append(quadTrees, TOP_LEFT)
-	}
-	if x < q.center.X && y+h > q.center.Y {
-		quadTrees = append(quadTrees, BOTTOM_LEFT)
-	}
-	if x+w > q.center.X && y < q.center.Y {
-		quadTrees = append(quadTrees, TOP_RIGHT)
-	}
-	if x+w > q.center.X && y+h > q.center.Y {
-		quadTrees = append(quadTrees, BOTTOM_RIGHT)
-	}
-
-	if len(quadTrees) == 0 {
-		return errors.New("Error inserting elements, could not find inserting quadtree")
-	}
-	if len(quadTrees) > 1 {
-		q.eles = append(q.eles, e)
-		e.SetQuadTree(q)
+	if len(q.Elements) < 3 && !q.hasSub {
+		q.Elements = append(q.Elements, e)
 		return nil
 	}
-	xQ, yQ := q.center.Get()
-	wQ, hQ := q.size.Get()
-	wQ = wQ / 2 //SUB QUADTREE WIDTH
-	hQ = hQ / 2 //SUB QUADTREE HEIGHT
-	switch quadTrees[0] {
-	case TOP_LEFT:
-		if q.top_left == nil {
-			q.top_left = NewQuadTree(base.Vec[float32]{X: xQ - wQ/2, Y: yQ - hQ/2}, base.Vec[float32]{X: wQ / 2, Y: hQ / 2}, q)
-		}
-		return q.top_left.Insert(e)
-	case TOP_RIGHT:
-		if q.top_right == nil {
-			q.top_right = NewQuadTree(base.Vec[float32]{X: xQ + wQ/2, Y: yQ - hQ/2}, base.Vec[float32]{X: wQ / 2, Y: hQ / 2}, q)
-		}
+	fmt.Printf("Reallocate %v\n", e.GetPos())
 
-		return q.top_right.Insert(e)
-	case BOTTOM_LEFT:
-		if q.bottom_left == nil {
-			q.bottom_left = NewQuadTree(base.Vec[float32]{X: xQ - wQ/2, Y: yQ + hQ/2}, base.Vec[float32]{X: wQ / 2, Y: hQ / 2}, q)
+	xQ, yQ := q.Pos.Get()
+	wQ, hQ := q.Size.Get()
+	centerX, centerY := xQ+wQ/2, yQ+hQ/2
+
+	createQuadTree := func(where string) (quadtree *QuadTree, alreadyExist bool) {
+		alreadyExist = true
+		switch where {
+		case TOP_LEFT:
+			if q.Top_left == nil {
+				q.Top_left = NewQuadTree(base.Vec[float32]{X: 0, Y: 0}, base.Vec[float32]{X: wQ / 2, Y: hQ / 2}, q)
+				alreadyExist = false
+			}
+			return q.Top_left, alreadyExist
+		case TOP_RIGHT:
+			if q.Top_right == nil {
+				q.Top_right = NewQuadTree(base.Vec[float32]{X: wQ / 2, Y: 0}, base.Vec[float32]{X: wQ / 2, Y: hQ / 2}, q)
+				alreadyExist = false
+			}
+
+			return q.Top_right, alreadyExist
+		case BOTTOM_LEFT:
+			if q.Bottom_left == nil {
+				q.Bottom_left = NewQuadTree(base.Vec[float32]{X: 0, Y: hQ / 2}, base.Vec[float32]{X: wQ / 2, Y: hQ / 2}, q)
+				alreadyExist = false
+			}
+			return q.Bottom_left, alreadyExist
+		case BOTTOM_RIGHT:
+			if q.Bottom_right == nil {
+				q.Bottom_right = NewQuadTree(base.Vec[float32]{X: wQ / 2, Y: hQ / 2}, base.Vec[float32]{X: wQ / 2, Y: hQ / 2}, q)
+				alreadyExist = false
+			}
+			return q.Bottom_right, alreadyExist
 		}
-		return q.bottom_left.Insert(e)
-	case BOTTOM_RIGHT:
-		if q.bottom_right == nil {
-			q.bottom_right = NewQuadTree(base.Vec[float32]{X: xQ + wQ/2, Y: yQ + hQ/2}, base.Vec[float32]{X: wQ / 2, Y: hQ / 2}, q)
-		}
-		return q.bottom_right.Insert(e)
+		return nil, false
 	}
+
+	elementsToReallocate := append(q.Elements, e)
+	q.Elements = make([]QuadTreeElement[float32], 0)
+	fmt.Println(centerX, centerY)
+	for i := range elementsToReallocate {
+		e := elementsToReallocate[i]
+		x, y := e.GetPos().Get()
+		w, h := e.GetBox().Get()
+		quadtreeToCreate := []string{}
+		if x < centerX && y < centerY {
+			quadtreeToCreate = append(quadtreeToCreate, TOP_LEFT)
+		}
+		if x < centerX && y+h > centerY {
+			quadtreeToCreate = append(quadtreeToCreate, BOTTOM_LEFT)
+		}
+		if x+w > centerX && y < centerY {
+			quadtreeToCreate = append(quadtreeToCreate, TOP_RIGHT)
+		}
+		if x+w > centerX && y+h > centerY {
+			quadtreeToCreate = append(quadtreeToCreate, BOTTOM_RIGHT)
+		}
+		fmt.Printf("%v -> %v\n", elementsToReallocate[i].GetPos(), quadtreeToCreate)
+		if len(quadtreeToCreate) == 0 {
+			return errors.New("Error inserting elements, could not find inserting quadtree")
+		}
+		if len(quadtreeToCreate) > 1 {
+			q.Elements = append(q.Elements, e)
+			e.SetQuadTree(q)
+			continue
+		}
+		quadtree, _ := createQuadTree(quadtreeToCreate[0])
+		if quadtree == nil {
+			return fmt.Errorf("Error creating quadtree in %v", quadtreeToCreate[0])
+		}
+		q.hasSub = true
+		err := quadtree.Insert(e)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
